403b16624ea8a669078b0bff048ae38571d7b58e
sihwan <stay136@gmail.com>
2025-08-25 22:38:28 +0900

    add: api, worker project init

---
+++ apps/worker/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.NODE_ENV}`],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.MYSQL_HOST,
+        port: parseInt(process.env.MYSQL_PORT || '3306', 10),
+        username: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DB,
+        autoLoadEntities: false,
+        synchronize: false,
+      }),
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI || ''),
+  ],
+})
+export class AppModule {}
